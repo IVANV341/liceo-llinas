2fe579738614b8df3f40ce20e7fd84cceab58130
Tu Nombre <tuemail@ejemplo.com>
2025-09-02 18:59:16 -0500

    Add root server.php router (matches Railway start cmd)

---
+++ server.php
@@ -0,0 +1,5 @@
+﻿<?php
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$publicPath = __DIR__ . '/public';
+if ($uri !== '/' && file_exists($publicPath . $uri)) { return false; }
+require $publicPath . '/index.php';
