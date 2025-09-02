ba3fa6766e40345c2b19928d288732babd6957e9
Tu Nombre <tuemail@ejemplo.com>
2025-09-02 18:56:21 -0500

    Rollback: remove router/Procfile and restore index

--- public/server.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-$publicPath = __DIR__ . '/public';
-if ($uri !== '/' && file_exists($publicPath . $uri)) {
-    return false; // sirve archivos estáticos tal cual
-}
-require_once $publicPath . '/index.php';
