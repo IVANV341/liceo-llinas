c42c3634c7786df8c9b48cc6e72a594f0e939109
Tu Nombre <tuemail@ejemplo.com>
2025-09-02 19:19:21 -0500

    Rollback: arrancar con public/index.php (sin router/Procfile)

--- public/server.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-// Router para el servidor embebido de PHP.
-// Si el archivo estático existe (png, jpg, css, js), se sirve tal cual.
-// Si no, se reescribe a index.php de Laravel.
-$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-if ($uri !== '/' && file_exists(__DIR__ . $uri)) {
-    return false;
-}
-require __DIR__ . '/index.php';
